2c5258c081d5ad6a79e5d403decc8830d7e3f833
roshh4 <roshinidharmaraj@gmail.com>
2024-09-09 11:38:29 +0530

    manual data insertion of book format association

---
+++ backend/src/main/resources/static/book_format_associations.sql
@@ -0,0 +1,85 @@
+-- F. Scott Fitzgerald
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (1, 1, 299.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (1, 2, 599.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (1, 4, 199.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (2, 1, 349.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (2, 2, 649.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (2, 3, 399.00);  -- Audiobook
+
+-- Harper Lee
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (3, 1, 399.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (3, 2, 699.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (3, 4, 249.00);  -- Ebook
+
+-- George Orwell
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (4, 1, 299.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (4, 3, 199.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (4, 4, 149.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (5, 1, 399.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (5, 4, 249.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (6, 1, 299.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (6, 2, 599.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (6, 3, 349.00);  -- Audiobook
+
+-- J.K. Rowling
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (7, 1, 499.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (7, 2, 799.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (7, 4, 349.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (8, 1, 549.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (8, 3, 449.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (8, 4, 399.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (9, 1, 599.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (9, 2, 899.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (9, 4, 499.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (10, 3, 249.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (10, 4, 349.00);  -- Ebook
+
+-- Jane Austen
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (11, 1, 349.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (11, 2, 649.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (11, 4, 299.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (12, 1, 399.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (12, 4, 249.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (13, 2, 699.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (13, 3, 499.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (14, 4, 199.00);  -- Ebook
+
+-- Ernest Hemingway
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (15, 1, 449.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (15, 2, 749.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (15, 3, 549.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (16, 1, 299.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (16, 4, 199.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (17, 1, 349.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (17, 2, 599.00);  -- Hardcover
+
+-- Mark Twain
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (18, 1, 199.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (18, 2, 499.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (18, 4, 149.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (19, 1, 399.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (20, 2, 549.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (20, 3, 299.00);  -- Audiobook
+
+-- Mary Shelley
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (21, 2, 699.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (21, 4, 249.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (22, 1, 449.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (22, 3, 349.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (23, 2, 749.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (23, 4, 299.00);  -- Ebook
+
+-- Charles Dickens
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (24, 1, 349.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (24, 2, 599.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (25, 1, 399.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (25, 4, 249.00);  -- Ebook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (26, 2, 649.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (26, 3, 399.00);  -- Audiobook
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (27, 1, 199.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (27, 3, 299.00);  -- Audiobook
+
+-- J.R.R. Tolkien
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (28, 1, 499.00);  -- Paperback
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (28, 2, 799.00);  -- Hardcover
+INSERT INTO book_format_associations (book_id, format_id, price) VALUES (29, 2, 849.00);  -- Hardcover
